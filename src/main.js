5c100d1a6cb2ab3200410098de0ccedcfad15de0
UnRunDead <me@unrundead.dev>
2021-05-26 17:21:08 +0200

    Added bulma and font-awesome

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store/index';
 
-createApp(App).use(store).use(router).mount("#app");
+import BaseCard from './components/UI/BaseCard';
+
+import '@/assets/styles/main.scss';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUser, faKey, faUserTag } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faUser);
+library.add(faKey);
+library.add(faUserTag);
+
+const app = createApp(App);
+
+app.component('base-card', BaseCard);
+app.component('font-awesome-icon', FontAwesomeIcon);
+
+app.use(store);
+app.use(router);
+app.mount('#app');
