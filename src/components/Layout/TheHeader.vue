26f3dc782baab110f43ffde52d1aa42f6575eb0e
UnRunDead <me@unrundead.dev>
2021-05-26 17:24:34 +0200

    Created header and navbar component

---
+++ src/components/Layout/TheHeader.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="container">
+    <img src="../../assets/images/acc-game-logo.png" />
+    <h1 class="is-size-3 has-text-primary">Server Editor</h1>
+  </div>
+</template>
---
+++ src/components/Layout/TheNavbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <button class="button is-danger is-light" @click="logout">Logout</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout() {
+      this.$store.dispatch('auth/logout');
+    }
+  }
+};
+</script>
