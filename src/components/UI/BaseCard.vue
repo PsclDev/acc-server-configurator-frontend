0fb5e798a596734cc83b1674ae0a69eabd92a521
UnRunDead <me@unrundead.dev>
2021-05-26 17:24:53 +0200

    Created base card and base form input component

---
+++ src/components/UI/BaseCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card" :style="'max-width:' + maxWidth + 'rem;'">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    maxWidth: {
+      type: String,
+      default: '40'
+    }
+  }
+};
+</script>
+
+<style scoped>
+.card {
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+  margin: 2rem auto;
+}
+</style>
---
+++ src/components/UI/BaseFormInput.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container">
+    <div class="container is-flex is-align-items-center mb-1">
+      <font-awesome-icon :icon="icon" class="mr-2" />
+      <p>{{ title }}</p>
+    </div>
+    <input
+      class="input"
+      :class="validCheck ? 'is-danger' : ''"
+      :type="inputType"
+      v-model.trim="inputValue"
+      @blur="$emit('inputValueChanged', this.inputValue)"
+    />
+    <p v-if="validCheck" class="has-text-danger">
+      {{ errorMessage }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    valid: {
+      type: String,
+      required: true
+    },
+    errorMessage: {
+      type: String,
+      required: true
+    },
+    inputType: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      inputValue: ''
+    };
+  },
+  computed: {
+    validCheck() {
+      return this.valid === 'invalid';
+    }
+  }
+};
+</script>
