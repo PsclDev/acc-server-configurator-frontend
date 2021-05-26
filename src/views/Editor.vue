88a7f7252e95326033c8835d11ac22595be3a185
UnRunDead <me@unrundead.dev>
2021-05-26 17:27:24 +0200

    Removed template stuff and added some logic to app.vue

---
+++ src/views/Editor.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>Editor</p>
+  <TheNavbar />
+</template>
+
+<script>
+import TheNavbar from '../components/Layout/TheNavbar';
+
+export default {
+  components: {
+    TheNavbar
+  }
+};
+</script>
