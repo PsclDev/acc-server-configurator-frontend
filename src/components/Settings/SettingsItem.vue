e0bb0f9dab34417e6ef220f6737886c1e01cbaed
UnRunDead <me@unrundead.dev>
2021-05-30 23:21:45 +0200

    Moved Settings.Vue in folder and split up

---
+++ src/components/Settings/SettingsItem.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container">
+    <h1 class="has-text-left is-size-5">Test Abc</h1>
+    <p class="has-text-left is-size-7 mb-2">
+      Hallo dies ist eine Testbeschreibung
+    </p>
+    <div class="container is-flex">
+      <font-awesome-icon
+        :icon="requiresAdmin ? 'user-shield' : 'user-cog'"
+        :class="requiresAdmin ? 'has-text-primary' : ''"
+        class="mr-2 is-align-self-center"
+      />
+      <input
+        class="input is-small"
+        :class="requiresAdmin ? 'is-primary' : ''"
+        type="text"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    permissions: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    valueType: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    requiresAdmin() {
+      if (this.permissions === 'admin') return true;
+      return false;
+    }
+  }
+};
+</script>
---
+++ src/components/Settings/SettingsList.vue
@@ -0,0 +1,34 @@
+<template>
+  <base-card>
+    <h1 class="has-text-weight-bold is-size-4" @click="toggleView">
+      Settings List
+    </h1>
+    <settings-item :class="isVisible ? '' : 'hide'"></settings-item>
+  </base-card>
+</template>
+
+<script>
+import SettingsItem from './SettingsItem';
+
+export default {
+  components: {
+    SettingsItem
+  },
+  data() {
+    return {
+      isVisible: false
+    };
+  },
+  methods: {
+    toggleView() {
+      this.isVisible = !this.isVisible;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.hide {
+  display: none;
+}
+</style>
