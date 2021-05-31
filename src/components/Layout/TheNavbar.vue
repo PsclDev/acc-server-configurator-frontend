a3b688a312039fa2e1151ab51a4ba48904650df9
UnRunDead <me@unrundead.dev>
2021-05-31 11:19:10 +0200

    New style for navbar

--- src/components/Layout/TheNavbar.vue
+++ src/components/Layout/TheNavbar.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="is-flex is-justify-content-center mb-5">
-    <a class="has-text-white mx-5" @click="settings"> Settings </a>
-    <a class="has-text-white mx-5" @click="results"> Results </a>
-    <a class="has-text-primary mx-5" @click="logout"> Logout </a>
+    <div class="width centerContent">
+      <a
+        :class="
+          activeTab === 'results' ? 'has-text-white' : 'has-text-grey-dark'
+        "
+        class="mx-5"
+        @click="results"
+      >
+        Results
+      </a>
+      <a
+        :class="
+          activeTab === 'settings' ? 'has-text-white' : 'has-text-grey-dark'
+        "
+        class="mx-5"
+        @click="settings"
+      >
+        Settings
+      </a>
+    </div>
+
+    <div class="width rightContent">
+      <a class="has-text-primary mx-5" @click="logout"> Logout </a>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      activeTab: ''
+    };
+  },
   methods: {
     settings() {
+      this.activeTab = 'settings';
       this.$router.push('/settings');
     },
     results() {
+      this.activeTab = 'results';
       this.$router.push('/results');
     },
     logout() {
@@ -21,3 +49,18 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.width {
+  width: 33%;
+}
+
+.centerContent {
+  text-align: center;
+  margin-left: 33%;
+}
+
+.rightContent {
+  text-align: right;
+}
+</style>
