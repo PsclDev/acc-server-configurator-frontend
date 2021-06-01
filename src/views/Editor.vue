73cdf15a238bfae66c54ac309bad28ed7058f1ac
UnRunDead <me@unrundead.dev>
2021-06-01 23:35:15 +0200

    Refactored Views

--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -1,14 +1,42 @@
 <template>
-  <TheNavbar />
-  <router-view class="mt-5"></router-view>
+  <div>
+    <settings-list title="Assist Rules" :data="assistRules" />
+    <settings-list title="Configuration" :data="configuration" />
+    <settings-list title="Event" :data="event" />
+    <settings-list title="Event Rules" :data="eventRules" />
+    <settings-list title="Settings" :data="settings" />
+    <button class="button is-primary is-medium">
+      Commit Changes to Server
+    </button>
+  </div>
 </template>
 
 <script>
-import TheNavbar from '../components/Layout/TheNavbar';
+import SettingsList from '../components/Settings/SettingsList';
 
 export default {
   components: {
-    TheNavbar
+    SettingsList
+  },
+  computed: {
+    assistRules() {
+      return this.$store.getters['editor/assistRules'];
+    },
+    configuration() {
+      return this.$store.getters['editor/configuration'];
+    },
+    event() {
+      return this.$store.getters['editor/event'];
+    },
+    eventRules() {
+      return this.$store.getters['editor/eventRules'];
+    },
+    settings() {
+      return this.$store.getters['editor/settings'];
+    }
+  },
+  async created() {
+    await this.$store.dispatch('editor/getEditorContent');
   }
 };
 </script>
---
+++ src/views/Results.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Results Vue</h1>
+</template>
