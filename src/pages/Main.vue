2137f4cde78d9848c250816652c5eb528fcd0d8c
UnRunDead <me@unrundead.dev>
2021-06-01 23:35:39 +0200

    Created pages folder and two pages

---
+++ src/pages/Main.vue
@@ -0,0 +1,14 @@
+<template>
+  <TheNavbar />
+  <router-view class="mt-5"></router-view>
+</template>
+
+<script>
+import TheNavbar from '../components/Layout/TheNavbar';
+
+export default {
+  components: {
+    TheNavbar
+  }
+};
+</script>
