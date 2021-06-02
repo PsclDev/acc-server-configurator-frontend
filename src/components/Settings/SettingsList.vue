f6ff9600791c640e333d5cf13dd26d57fc1efeb4
UnRunDead <me@unrundead.dev>
2021-06-02 16:06:12 +0200

    Reworked settingslist

--- src/components/Settings/SettingsList.vue
+++ src/components/Settings/SettingsList.vue
@@ -1,32 +1,14 @@
 <template>
-  <base-card class="boxStyling">
-    <div class="is-flex is-justify-content-center">
-      <div class="width centerContent">
-        <h1 class="has-text-weight-bold is-size-4" @click="toggleView">
-          {{ title }}
-        </h1>
-      </div>
-
-      <div class="width rightContent">
-        <font-awesome-icon
-          :icon="isVisible ? 'chevron-up' : 'chevron-down'"
-          class="mr-2"
-        />
-      </div>
-    </div>
-    <div :class="isVisible ? '' : 'hide'">
-      <settings-item
-        class="mb-3"
-        v-for="entry in data"
-        :key="entry.name"
-        :name="entry.name"
-        :description="entry.description"
-        :value="entry.value"
-        :valueType="entry.value_type"
-        :requiredPermissions="entry.required_permissions"
-      />
-    </div>
-  </base-card>
+  <settings-item
+    class="mb-3"
+    v-for="entry in data"
+    :key="entry.name"
+    :name="entry.name"
+    :description="entry.description"
+    :value="entry.value"
+    :valueType="entry.value_type"
+    :requiredPermissions="entry.required_permissions"
+  />
 </template>
 
 <script>
@@ -34,10 +16,6 @@ import SettingsItem from './SettingsItem';
 
 export default {
   props: {
-    title: {
-      type: String,
-      required: true
-    },
     data: {
       type: Array,
       required: true
@@ -45,41 +23,6 @@ export default {
   },
   components: {
     SettingsItem
-  },
-  data() {
-    return {
-      isVisible: false
-    };
-  },
-  methods: {
-    toggleView() {
-      this.isVisible = !this.isVisible;
-    }
   }
 };
 </script>
-
-<style scoped>
-.boxStyling {
-  background-color: #4e5172;
-  color: #f8f8ff;
-}
-
-.hide {
-  display: none;
-}
-
-.width {
-  width: 33%;
-}
-
-.centerContent {
-  text-align: center;
-  margin-left: 33%;
-}
-
-.rightContent {
-  text-align: right;
-  align-self: center;
-}
-</style>
