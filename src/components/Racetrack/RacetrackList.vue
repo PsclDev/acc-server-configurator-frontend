e799cda166804c2d747131c89ccce552a1ee51fd
UnRunDead <me@unrundead.dev>
2021-06-02 16:05:55 +0200

    Added Racetrack List and Item as component

---
+++ src/components/Racetrack/RacetrackList.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container is-flex is-flex-wrap-wrap">
+    <racetrack-item
+      class="mb-5"
+      v-for="track in getTracks"
+      :key="track.name"
+      :selected="track.selected"
+      :name="track.name"
+      :section="track.section"
+      :trackimage="track.image"
+      :location="track.location"
+      :km="track.km"
+      :dlc="track.dlc"
+      @trackSelected="trackSelected"
+    />
+  </div>
+</template>
+
+<script>
+import RacetrackItem from './RacketrackItem';
+import { tracks } from '../../assets/data/racetracks.json';
+
+export default {
+  components: {
+    RacetrackItem
+  },
+  data() {
+    return {
+      tracks
+    };
+  },
+  computed: {
+    getTracks() {
+      return this.tracks;
+    }
+  },
+  methods: {
+    setTracks(newTracks) {
+      this.tracks = newTracks;
+      console.log(this.tracks);
+    },
+    trackSelected(trackName) {
+      let tmpArr = [];
+
+      for (const idx in tracks) {
+        let track = tracks[idx];
+
+        if (track.name === trackName) track.selected = true;
+        else track.selected = false;
+        tmpArr.push(track);
+      }
+      this.setTracks(tmpArr);
+    }
+  }
+};
+</script>
---
+++ src/components/Racetrack/RacketrackItem.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="container has-text-white is-flex is-justify-content-center bg">
+    <img class="racetrackImage" :src="racetrackImage" />
+    <div>
+      <div class="is-flex is-align-items-center">
+        <img class="flagImage mr-4" :src="flagImage" />
+        <h1 class="has-text-weight-bold">{{ name }}</h1>
+      </div>
+      <div class="mt-3">
+        <p class="has-text-left">Section: {{ section }}</p>
+        <p class="has-text-left">KM: {{ km }}</p>
+        <p
+          class="has-text-left"
+          :class="dlc !== 'None' ? 'has-text-link has-text-weight-bold' : ''"
+        >
+          DLC Required: {{ dlc }}
+        </p>
+        <a
+          v-if="!isSelected"
+          class="button is-outlined is-link mt-4"
+          @click="selectTrack"
+          >Select Track</a
+        >
+        <a v-else class="button is-primary mt-4" @click="selectTrack"
+          >Selected</a
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    selected: {
+      type: Boolean,
+      default: false
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    section: {
+      type: String,
+      required: true
+    },
+    trackimage: {
+      type: String,
+      required: true
+    },
+    location: {
+      type: String,
+      required: true
+    },
+    km: {
+      type: String,
+      required: true
+    },
+    dlc: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    isSelected() {
+      return this.selected;
+    },
+    racetrackImage() {
+      return require(`../../assets/images/racetracks/${this.trackimage}`);
+    },
+    flagImage() {
+      return require(`../../assets/images/country-flags/${this.location}.svg`);
+    }
+  },
+  methods: {
+    selectTrack() {
+      this.isSelected = !this.isSelected;
+      this.$emit('trackSelected', this.name);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.bg {
+  background-color: #4e5172;
+}
+
+.racetrackImage {
+  align-self: center;
+  width: 200px;
+  height: 200px;
+}
+
+.flagImage {
+  max-width: 15%;
+}
+</style>
