a5f6a2dfc5787fb89a22eeac3fb211616aa196a4
UnRunDead <me@unrundead.dev>
2021-06-04 23:10:59 +0200

    Bugfix in global store index.js

--- src/store/index.js
+++ src/store/index.js
@@ -4,13 +4,15 @@ import appModule from './modules/app/index'
 import authModule from './modules/auth/index';
 import editorModule from './modules/editor/index';
 import entryListModule from './modules/entrylist/index'
+import resultsModule from './modules/results/index'
 
 const store = createStore({
   modules: {
     app: appModule,
     auth: authModule,
     editor: editorModule,
-    entryList: entryListModule
+    entryList: entryListModule,
+    results: resultsModule
   }
 });
 
