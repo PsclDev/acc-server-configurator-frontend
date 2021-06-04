26b585c58bbca0a718496c99a71d6f89a6137955
UnRunDead <me@unrundead.dev>
2021-06-04 23:10:35 +0200

    Bugfix in router.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import store from '../store/index';
 import Editor from '../views/Editor.vue';
-import Results from '../views/Results.vue';
+import Resultslist from '../views/Resultslist.vue';
 import Entrylist from '../views/Entrylist.vue'
 
 const routes = [
@@ -9,7 +9,7 @@ const routes = [
     path: '/settings', component: Editor
   },
   {
-    path: '/results', component: Results
+    path: '/results', component: Resultslist
   },
   {
     path: '/entrylist', component: Entrylist
