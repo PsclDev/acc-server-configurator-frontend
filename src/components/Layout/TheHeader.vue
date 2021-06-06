47a5118769463a8c96f8bfef37cf59cd29ef6f2a
UnRunDead <me@unrundead.dev>
2021-06-06 22:24:22 +0200

    Added donation button to header and a tag for alpha status of the app

--- src/components/Layout/TheHeader.vue
+++ src/components/Layout/TheHeader.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="container">
     <img src="../../assets/images/acc-game-logo.png" />
-    <h1 class="is-size-3 has-text-primary">Server Editor</h1>
+    <h1 class="is-size-3 has-text-primary">Server Editor (Alpha)</h1>
+    <a
+      class="has-text-white"
+      href="http://buymeacoffee.com/unrundead"
+      target="_blank"
+    >
+      You like what I did? Click Me to buy me a Coffee :) Thanks
+    </a>
   </div>
 </template>
