612c8209bac0e4cb530b877decf807d51a1c6edc
UnRunDead <me@unrundead.dev>
2021-06-16 22:38:32 +0200

    Added penalty and lap to car item

--- src/components/Results/CarItem.vue
+++ src/components/Results/CarItem.vue
@@ -2,16 +2,30 @@
   <p>Teamname: Porsche Werks Team</p>
   <p>Car: Porsche</p>
   <p>Racenumber: 911</p>
+  <p>Best Lap: 1:00:00</p>
+  <p>Best Split</p>
+  <p>Split 1: 00:20:00</p>
+  <p>Split 2: 00:20:00</p>
+  <p>Split 3: 00:20:00</p>
+  <p>Total Time: 01:00:00</p>
+  <p>Lap Count: 1</p>
+  <p>Mandatory Pitstop missed?: No</p>
 
   <driver-item v-for="driver in drivers" :key="driver" />
+  <lap-item v-for="driver in drivers" :key="driver" />
+  <penalty-item v-for="driver in drivers" :key="driver" />
 </template>
 
 <script>
 import DriverItem from './DriverItem.vue';
+import LapItem from './LapItem.vue';
+import PenaltyItem from './PenaltyItem.vue';
 
 export default {
   components: {
-    DriverItem
+    DriverItem,
+    LapItem,
+    PenaltyItem
   },
   data() {
     return {
