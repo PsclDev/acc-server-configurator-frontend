89d848d5741ab88e315a53d7246df248644172a1
UnRunDead <me@unrundead.dev>
2021-06-16 22:37:32 +0200

    Added lap item for results

---
+++ src/components/Results/LapItem.vue
@@ -0,0 +1,9 @@
+<template>
+  <p>Driver: Max Mustermann</p>
+  <p>Lap Time: 01:00:00</p>
+  <p>Best Split</p>
+  <p>Split 1: 00:20:00</p>
+  <p>Split 2: 00:20:00</p>
+  <p>Split 3: 00:20:00</p>
+  <p>Valid Lap?: Yes</p>
+</template>
