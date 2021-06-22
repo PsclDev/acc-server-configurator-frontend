725544e726f46221e0e97941c7ad7a38ad1d279c
UnRunDead <me@unrundead.dev>
2021-06-22 22:16:17 +0200

    Added getCars action and getter

--- src/store/modules/results/actions.js
+++ src/store/modules/results/actions.js
@@ -13,5 +13,24 @@ export default {
         });
 
         context.commit('setResults', { list: response.data })
+    },
+    async loadCars(context, payload) {
+        const resultId = payload.id;
+        const baseURL = process.env.VUE_APP_API_URL;
+        const url = `${baseURL}/results/${resultId}/cars`;
+        const token = localStorage.getItem('token');
+
+        var response = await axios.get(url, {
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        })
+
+        let resultList = context.rootGetters['results/getResults'];
+        const resultIdx = resultList.findIndex(entry => entry.id === resultId)
+        const result = resultList[resultIdx];
+        result.cars = response.data;
+        resultList[resultIdx] = result;
+        context.commit('setResults', { list: resultList })
     }
 };
--- src/store/modules/results/getters.js
+++ src/store/modules/results/getters.js
@@ -1,5 +1,8 @@
 export default {
     getResults(state) {
         return state.results;
+    },
+    getCars: (state) => (id) => {
+        return state.results.filter((result) => result.id === id)[0].cars;
     }
 };
