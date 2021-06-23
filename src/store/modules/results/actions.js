5ec5ee2318eb00bebda77ebd999e8139600cf5dd
UnRunDead <me@unrundead.dev>
2021-06-23 23:25:43 +0200

    enhanced loading method and added laps and penalties inc. getters

--- src/store/modules/results/actions.js
+++ src/store/modules/results/actions.js
@@ -14,10 +14,12 @@ export default {
 
         context.commit('setResults', { list: response.data })
     },
-    async loadCars(context, payload) {
+    async loadItem(context, payload) {
         const resultId = payload.id;
+        const obj = payload.obj;
+        const targetPath = payload.targetURL;
         const baseURL = process.env.VUE_APP_API_URL;
-        const url = `${baseURL}/results/${resultId}/cars`;
+        const url = `${baseURL}/results/${targetPath}`;
         const token = localStorage.getItem('token');
 
         var response = await axios.get(url, {
@@ -29,8 +31,27 @@ export default {
         let resultList = context.rootGetters['results/getResults'];
         const resultIdx = resultList.findIndex(entry => entry.id === resultId)
         const result = resultList[resultIdx];
-        result.cars = response.data;
+        result[obj] = response.data;
         resultList[resultIdx] = result;
         context.commit('setResults', { list: resultList })
+
+    },
+    async loadCars(context, payload) {
+        payload.targetURL = `${payload.id}/cars`;
+        payload.obj = 'cars';
+        await context.dispatch('loadItem',
+            payload)
+    },
+    async loadLaps(context, payload) {
+        payload.targetURL = `${payload.id}/${payload.carId}/laps`;
+        payload.obj = 'laps';
+        await context.dispatch('loadItem',
+            payload)
+    },
+    async loadPenalties(context, payload) {
+        payload.targetURL = `${payload.id}/${payload.carId}/penalties`;
+        payload.obj = 'penalties';
+        await context.dispatch('loadItem',
+            payload)
     }
 };
--- src/store/modules/results/getters.js
+++ src/store/modules/results/getters.js
@@ -4,5 +4,11 @@ export default {
     },
     getCars: (state) => (id) => {
         return state.results.filter((result) => result.id === id)[0].cars;
+    },
+    getLaps: (state) => (id) => {
+        return state.results.filter((result) => result.id === id)[0].laps;
+    },
+    getPenalties: (state) => (id) => {
+        return state.results.filter((result) => result.id === id)[0].penalties;
     }
 };
