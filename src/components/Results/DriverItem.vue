7174c73bd18cbc3c64f67256dbf054456a58700e
UnRunDead <me@unrundead.dev>
2021-06-25 22:42:28 +0200

    styled driver item

--- src/components/Results/DriverItem.vue
+++ src/components/Results/DriverItem.vue
@@ -1,6 +1,15 @@
 <template>
-  <p>{{ driver.name }}</p>
-  <p>{{ driver.shortName }}</p>
+  <base-card class="py-1 px-4">
+    <div class="is-flex is-flex-direction-column">
+      <p class="has-text-weight-bold is-size-7">Fullname:</p>
+      <p>{{ driver.name }}</p>
+    </div>
+
+    <div class="is-flex is-flex-direction-column">
+      <p class="has-text-weight-bold is-size-7">Shortname:</p>
+      <p>{{ driver.shortName }}</p>
+    </div>
+  </base-card>
 </template>
 
 <script>
