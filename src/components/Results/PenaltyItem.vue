5947a16015456cf84f34629667f12b0dd6bb3f8b
UnRunDead <me@unrundead.dev>
2021-06-26 22:10:44 +0200

    styled penalty item

--- src/components/Results/PenaltyItem.vue
+++ src/components/Results/PenaltyItem.vue
@@ -1,9 +1,41 @@
 <template>
-  <p>{{ driverName }}</p>
-  <p>{{ penalty.reason }}</p>
-  <p>{{ penalty.penalty }}</p>
-  <p>{{ penalty.violationInLap }}</p>
-  <p>{{ penalty.clearedInLap }}</p>
+  <base-card class="py-2 px-5 is-flex">
+    <div
+      class="is-flex is-flex-direction-column is-justify-content-space-around"
+    >
+      <div class="is-flex is-flex-direction-column">
+        <p class="has-text-weight-bold is-size-7">Reason:</p>
+        <p>{{ penalty.reason }}</p>
+      </div>
+
+      <div class="is-flex is-flex-direction-column">
+        <p class="has-text-weight-bold is-size-7">Penalty:</p>
+        <p>{{ penalty.penalty }}</p>
+      </div>
+    </div>
+
+    <div
+      class="
+        ml-5
+        is-flex is-flex-direction-column is-justify-content-space-around
+      "
+    >
+      <div class="is-flex is-flex-direction-column is-size-7">
+        <p class="has-text-weight-bold">Driver:</p>
+        <p>{{ driverName }}</p>
+      </div>
+
+      <div class="is-flex is-flex-direction-column is-size-7">
+        <p class="has-text-weight-bold">Violation in Lap:</p>
+        <p>{{ penalty.violationInLap }}</p>
+      </div>
+
+      <div class="is-flex is-flex-direction-column is-size-7">
+        <p class="has-text-weight-bold">Cleared in Lap:</p>
+        <p>{{ penalty.clearedInLap }}</p>
+      </div>
+    </div>
+  </base-card>
 </template>
 
 <script>
